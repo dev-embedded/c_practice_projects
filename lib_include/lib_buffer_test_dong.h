e2053f8d0dfb0698d8d1e918f78178e99d825331
Dong LING <canada.dongling@gmail.com>
2015-06-14 21:34:50 -0400

    Establish: buffer library project.
    Establish lib_buffer_test_dong.h and decision structures' members' name according to POSIX naming.

--- lib_include/lib_buffer_test_dong.h
+++ lib_include/lib_buffer_test_dong.h
@@ -8,4 +8,29 @@
 
 void lib_buffer_test_dong();
 
+void lib_buffer_create();
+void lib_buffer_free();
+
+void lib_buffer_write();
+void lib_buffer_read();
+
+struct lib_buffer{
+	int lb_data;
+	struct lib_buffer *next;
+	struct lib_buffer *prev;
+};
+
+void singly_linked_list_create();
+void singly_linked_list_delete();
+
+int singly_linked_list_length();
+void singly_linked_list_insert();
+void singly_linked_list_sort();
+void singly_linked_list_show();
+
+struct singly_linked_list{
+	int sll_data;
+	struct singly_linked_list *next;
+};
+
 #endif
--- lib_src/lib_buffer_test_dong.c
+++ lib_src/lib_buffer_test_dong.c
@@ -1,5 +1,11 @@
 #include <string.h>
 
+typedef struct Singal_List_Linked
+{
+	int data;
+	struct Singal_List_Linked *next;
+}
+
 void lib_buffer_test_dong()
 {
 	return 0;
